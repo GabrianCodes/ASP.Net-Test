52fb8211e36aa473d628d2a7ded7d3fa22904dea
lozanogabrian <lozanogabrian@gmail.com>
2024-07-22 15:46:35 +0300

    added s01

---
+++ s01/Discussion/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Discussion.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // this one will render the page
+        }
+        public IActionResult CreateCourse()
+        {
+            return View();
+        }
+
+    }
+}
